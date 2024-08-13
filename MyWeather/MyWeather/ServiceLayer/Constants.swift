3104ba626ff1c5c4749d8708551e0848dd38b2aa
kiasha10 <kiasharangasamy2000@gmail.com>
2024-08-13 11:09:11 +0200

    Added Constants

--- MyWeather/MyWeather/ServiceLayer/Constants.swift
+++ MyWeather/MyWeather/ServiceLayer/Constants.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+struct Endpoint {
+    
+    static let weatherForecast = "https://api.openweathermap.org/data/2.5/forecast?lat=44.34&lon=10.99&appid=ca10f5419e656a65370b3e4f81a2ccc0"
+}
+
