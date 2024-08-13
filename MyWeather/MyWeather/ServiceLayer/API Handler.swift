d2aa22c8758ea484eadf18f5e3b36346fef5770b
kiasha10 <kiasharangasamy2000@gmail.com>
2024-08-13 09:30:21 +0200

    Added serviceErrors

---
+++ MyWeather/MyWeather/ServiceLayer/API Handler.swift
@@ -0,0 +1,8 @@
+//
+//  API Handler.swift
+//  MyWeather
+//
+//  Created by Kiasha Rangasamy on 2024/08/13.
+//
+
+import Foundation
---
+++ MyWeather/MyWeather/ServiceLayer/Constants.swift
@@ -0,0 +1,8 @@
+//
+//  Constants.swift
+//  MyWeather
+//
+//  Created by Kiasha Rangasamy on 2024/08/13.
+//
+
+import Foundation
