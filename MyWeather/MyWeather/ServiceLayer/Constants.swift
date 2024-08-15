87a2d296f7f37b9f07b388ef490c7a04d010f597
kiasha10 <kiasharangasamy2000@gmail.com>
2024-08-15 18:53:16 +0200

    Restructuring of home screen storyboard
    1. Modified assets file
    2. Added configuration for weather backgrounds, icons and colour
    3. Made edits to Xib cell
    4. Modified HomeScreen Storyboard, ViewModel and ViewController

--- MyWeather/MyWeather/ServiceLayer/Constants.swift
+++ MyWeather/MyWeather/ServiceLayer/Constants.swift
@@ -6,6 +6,7 @@
 //
 
 import Foundation
+import UIKit
 
 struct Endpoint {
     
@@ -18,3 +19,45 @@ struct TableViewIdentifiers {
     static let WeatherHomeScreenTableViewCell = "WeatherHomeScreenTableViewCell"
 }
 
+struct WeatherResponses {
+    
+    static func weatherCondition(for condition: Int) -> String {
+        switch condition {
+        case 500...504: return "Rain"
+        case 800: return "Sunny"
+        case 801...804: return "Cloudy"
+        default: return "Sunny"
+        }
+    }
+    
+    static func weatherIcons(for condition: Int) -> UIImage {
+        switch condition {
+        case 500...504: return UIImage(named: "Rain") ?? UIImage()
+        case 801...804: return UIImage(named: "partlyCloudy") ?? UIImage()
+        case 800: return UIImage(named: "Clear") ?? UIImage()
+        default: return UIImage(named: "Clear") ?? UIImage()
+        }
+    }
+    
+    static func backgroundWeatherTheme(for condition: Int) -> UIImage {
+        switch condition {
+        case 500...504: return UIImage(named: "forest_rainy") ?? UIImage()
+        case 801...804: return UIImage(named: "forest_cloudy") ?? UIImage()
+        case 800: return UIImage(named: "forest_sunny") ?? UIImage()
+        default: return UIImage(named: "forest_sunny") ?? UIImage()
+        }
+    }
+    
+    static func backgroundThemeColour(for condition: Int) -> UIColor {
+        switch condition {
+        case 500...504: return UIColor.rainy
+        case 801...804: return UIColor.cloudy
+        case 800: return UIColor.sunny
+        default: return UIColor.sunny
+        }
+    }
+}
+    
+func fahrenheitToCelsius(_ fahrenheit: Double) -> Double {
+        return (fahrenheit - 32) * 5 / 9
+    }
